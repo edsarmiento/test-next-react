9a454a8e8ffb3e450a49f2f2ba6298ecac65f6de
David ST <edst83@gmail.com>
2026-07-28 22:06:59 -0600

    working

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,37 @@
+"use client";
+
+import {useState, useEffect} from 'react'
 import {RandomFox} from '../components/RandomFox';
 
-let randomId = Math.floor(Math.random() * 123) + 1;
-
-
-
 export default function Home() {
-  return (
+
+  const [images, setImages] = useState<string[]>([]);
+
+  useEffect(() => {
+    const generateImages = () =>
+      Array(4).fill(null).map(() => 
+        `https://randomfox.ca/images/${Math.floor(Math.random() * 123) + 1}.jpg`
+      );
+    
+    setImages(generateImages());
+  }, []);
+
+    return(
     <div>
       <main className="flex min-h-screen items-center justify-center">
+
         <h1 className="text-5xl font-bold text-blue-600">
           Tailwind is working!
         </h1>
         
         <div>
-          <RandomFox image={`https://randomfox.ca/images/${randomId}.jpg`}  />
+          {images.map((image, index) => (
+            <div key={index} className="p-4">
+              <RandomFox image={image} />
+            </div>
+          ))}
         </div>
+          
       </main>
     </div>
   );
